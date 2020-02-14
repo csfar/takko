eb90d5db19c5fb402e74e789ee9c11959cb5a63c
Artur Carneiro <csf.artur@gmail.com>
2020-02-14 16:02:54 -0300

    Initial Commit

---
+++ takko/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  takko
+//
+//  Created by Artur Carneiro on 14/02/20.
+//  Copyright © 2020 Artur Carneiro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
